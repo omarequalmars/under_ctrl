a02d57f30e40814952efd2ac71e491ecc240395d
omarequalmars <90857121+omarequalmars@users.noreply.github.com>
2022-11-18 16:37:36 +0200

    Update DCMotorHBridge.h

--- DCMotorHBridge.h
+++ DCMotorHBridge.h
@@ -9,7 +9,7 @@ void MoveitMoveit(int PulseWidth, int A_channel, int B_channel, int INPUT_PIN){
         digitalWrite(B_channel,0);
         digitalWrite(INPUT_PIN,PulseWidth);
 
-    if(PulseWidth > 0{
+    if(PulseWidth > 0{ // CW
         digitalWrite(A_channel,0);
         digitalWrite(B_channel,1);
         digitalWrite(INPUT_PIN,PulseWidth);
