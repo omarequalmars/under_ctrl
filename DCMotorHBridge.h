1eaa6bf25f60267a4481837b96d64b335aea74b1
omarequalmars <90857121+omarequalmars@users.noreply.github.com>
2022-11-18 16:37:10 +0200

    Reduced number of needed inputs
    Modified the Motor-actuating function so that rotation direction is automatically inferred from the control signal sign

--- DCMotorHBridge.h
+++ DCMotorHBridge.h
@@ -1,15 +1,15 @@
 
 
-void MoveitMoveit(int PulseWidth, int direction, int A_channel, int B_channel, int INPUT_PIN){
+void MoveitMoveit(int PulseWidth, int A_channel, int B_channel, int INPUT_PIN){
     if (fabs(PulseWidth)>=255){
         PulseWidth = 255;
     }
-    if(direction==1){ // CCW
+    if(PulseWidth < 0){ // CCW
         digitalWrite(A_channel,1);
         digitalWrite(B_channel,0);
         digitalWrite(INPUT_PIN,PulseWidth);
 
-    if(direction==0){
+    if(PulseWidth > 0{
         digitalWrite(A_channel,0);
         digitalWrite(B_channel,1);
         digitalWrite(INPUT_PIN,PulseWidth);
@@ -17,8 +17,9 @@ void MoveitMoveit(int PulseWidth, int direction, int A_channel, int B_channel, i
     }
 }
 
-void rampspeed(int INPUT_PIN){
-    for(int i=0;i<255;i+=50){
-        digitalWrite(INPUT_PIN,i);
+void rampspeed( int increment, int delay_time, int A_channel, int B_channel,int INPUT_PIN){
+    for(int i=0;i<255;i+=increment){
+        MoveitMoveit(i, A_channel, B_channel, INPUT_PIN);
+	delay(delay_time);
     }
     }
