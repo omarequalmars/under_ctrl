123896ccbe511f0f3f83e089ebff34bc0aa25244
omarequalmars <90857121+omarequalmars@users.noreply.github.com>
2022-11-22 19:07:48 +0200

    Added more sensor-related functions

--- DCMotorHBridge.h
+++ DCMotorHBridge.h
@@ -1,25 +1,35 @@
 
 
 void MoveitMoveit(int PulseWidth, int A_channel, int B_channel, int INPUT_PIN){
-    if (fabs(PulseWidth)>=255){
-        PulseWidth = 255;
+  int pulse_mag = fabs(PulseWidth);
+  // Saturate output at 255
+    if (pulse_mag>=255){
+        pulse_mag = 255;
     }
+    
+    // decide directions based on PWM sign
     if(PulseWidth < 0){ // CCW
-        digitalWrite(A_channel,1);
-        digitalWrite(B_channel,0);
-        digitalWrite(INPUT_PIN,PulseWidth);
-
-    if(PulseWidth > 0{ // CW
-        digitalWrite(A_channel,0);
-        digitalWrite(B_channel,1);
-        digitalWrite(INPUT_PIN,PulseWidth);
+        digitalWrite(A_channel,HIGH);
+        digitalWrite(B_channel,LOW);
+        digitalWrite(INPUT_PIN,pulse_mag);
     }
+    else if(PulseWidth > 0){ // CW
+        digitalWrite(A_channel,LOW);
+        digitalWrite(B_channel,HIGH);
+        digitalWrite(INPUT_PIN,pulse_mag);
     }
+    
 }
 
-void rampspeed( int increment, int delay_time, int A_channel, int B_channel,int INPUT_PIN){
-    for(int i=0;i<255;i+=increment){
-        MoveitMoveit(i, A_channel, B_channel, INPUT_PIN);
-	delay(delay_time);
-    }
-    }
+float currentscale(int bits){
+    float V_0 = 2.5;
+    float voltage = bits*5/1023.0;
+    float current = ((voltage-V_0)/0.185);
+    return current;
+}
+
+float NeedForSpeed(int pulses, float Pulses_per_rev, float T_sampling){
+  float Speed;
+  Speed = 2*pulses/(Pulses_per_rev*T_sampling);
+  return Speed;
+}
