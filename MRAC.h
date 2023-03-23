b6d414632d55f1c2f40223d88bf7157491c9a564
omarequalmars <90857121+omarequalmars@users.noreply.github.com>
2023-03-23 14:30:17 +0200

    Update MRAC.h

--- MRAC.h
+++ MRAC.h
@@ -2,10 +2,13 @@
 
 float modelrefO1(float time_constant, float setpoint, float T_samp){
     static float state = 0;
-    static float A_m = B_m = 1/time_constant;
+    static float B_m = 1/time_constant
+    static float A_m = 1/time_constant;
+    static float last_setpoint = 0;
     static float a = B_m*T_samp/(2+A_m*T_samp);
     static float b = (A_m*T - 2)/(A_m*T + 2);
     state = a*(setpoint + last_setpoint) - b*state;
+    last_setpoint = setpoint;
     return state;
 }
 
